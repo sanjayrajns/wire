36e5756cf2d22d9c00851c756db3c21bb767b7c9
Sanjay Raj <157554250+sanjayrajns@users.noreply.github.com>
2026-09-03 17:34:23 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import pg from "pg"
 export const pool = new pg.Pool({
-    connectionString: "postgresql://neondb_owner:npg_auQY7giXLV2R@ep-autumn-snow-ay6t15dm-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    connectionString:process.env.CONNECTION_STRING,
     ssl: true
 }) ;
 
